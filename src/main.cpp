6ad36025751c1d6028f2b938c7a75f068269df62
Mateus Auler <mateusauler@protonmail.com>
2021-03-06 20:50:53 -0300

    main: Criada funcao para descobrir o nome do token pelo tipo

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,7 @@ using namespace std;
                           (c == '$' || c == '%' || c == ']' || c == '_' || c == '}'))
 
 token_t proximo_token();
+string nome_tipo_token(token_type_t tipo);
 
 FILE *f;
 int num_linha = 1;
@@ -78,183 +79,16 @@ main(int argc, char* argv[])
 	{
 		int old_linha = num_linha;
 		tok = proximo_token();
-		string tkname = "ERRO";
-
-		switch (tok.tipo)
-		{
-			case TK_ID:
-				tkname = "ID";
-				break;
-
-			case TK_CONST:
-				tkname = "CONST";
-				break;
-
-			case TK_OP_ATTRIB:
-				tkname = "OP_ATTRIB";
-				break;
-
-			case TK_OP_LT:
-				tkname = "OP_LT";
-				break;
-
-			case TK_OP_GT:
-				tkname = "OP_GT";
-				break;
-
-			case TK_OP_LE:
-				tkname = "OP_LE";
-				break;
-
-			case TK_OP_GE:
-				tkname = "OP_GE";
-				break;
-
-			case TK_OP_EQ:
-				tkname = "OP_EQ";
-				break;
-
-			case TK_OP_NE:
-				tkname = "OP_NE";
-				break;
-
-			case TK_OP_PLUS:
-				tkname = "OP_PLUS";
-				break;
-
-			case TK_OP_MINUS:
-				tkname = "OP_MINUS";
-				break;
-
-			case TK_OP_MUL:
-				tkname = "OP_MUL";
-				break;
-
-			case TK_OP_SLASH:
-				tkname = "OP_SLASH";
-				break;
-
-			case TK_OP_PERCENT:
-				tkname = "OP_PERCENT";
-				break;
-
-			case TK_BRA_O_PAR:
-				tkname = "BRA_O_PAR";
-				break;
-
-			case TK_BRA_C_PAR:
-				tkname = "BRA_C_PAR";
-				break;
-
-			case TK_BRA_O_SQR:
-				tkname = "BRA_O_SQR";
-				break;
-
-			case TK_BRA_C_SQR:
-				tkname = "BRA_C_SQR";
-				break;
-
-			case TK_BRA_O_CUR:
-				tkname = "BRA_O_CUR";
-				break;
-
-			case TK_BRA_C_CUR:
-				tkname = "BRA_C_CUR";
-				break;
-
-			case TK_OP_COMMA:
-				tkname = "OP_COMMA";
-				break;
-
-			case TK_RES_FINAL:
-				tkname = "RES_FINAL";
-				break;
-
-			case TK_RES_INT:
-				tkname = "RES_INT";
-				break;
-
-			case TK_RES_CHAR:
-				tkname = "RES_CHAR";
-				break;
-
-			case TK_RES_BOOLEAN:
-				tkname = "RES_BOOLEAN";
-				break;
-
-			case TK_RES_IF:
-				tkname = "RES_IF";
-				break;
-
-			case TK_RES_ELSE:
-				tkname = "RES_ELSE";
-				break;
-
-			case TK_RES_THEN:
-				tkname = "RES_THEN";
-				break;
-
-			case TK_RES_WHILE:
-				tkname = "RES_WHILE";
-				break;
-
-			case TK_RES_FOR:
-				tkname = "RES_FOR";
-				break;
-
-			case TK_RES_AND:
-				tkname = "RES_AND";
-				break;
-
-			case TK_RES_OR:
-				tkname = "RES_OR";
-				break;
-
-			case TK_RES_NOT:
-				tkname = "RES_NOT";
-				break;
-
-			case TK_RES_FALSE:
-				tkname = "RES_FALSE";
-				break;
-
-			case TK_RES_TRUE:
-				tkname = "RES_TRUE";
-				break;
-
-			case TK_RES_WRITE:
-				tkname = "RES_WRITE";
-				break;
-
-			case TK_RES_WRITELN:
-				tkname = "RES_WRITELN";
-				break;
-
-			case TK_RES_READLN:
-				tkname = "RES_READLN";
-				break;
-
-			case TK_RES_MAIN:
-				tkname = "RES_MAIN";
-				break;
-
-			case TK_END_STATEMENT:
-				tkname = "END_STATEMENT";
-				break;
-
-			case TK_EOF:
-				tkname = "EOF";
-				break;
-		}
 
 		if (old_linha != num_linha)
 			cout << endl << num_linha << '\t';
 
-		cout << tkname << ": \"" << tok.lex << "\"\t";
+		if (tok.tipo != TK_EOF)
+			cout << nome_tipo_token(tok.tipo) << ": \"" << tok.lex << "\"\t";
+		else
+			cout << nome_tipo_token(tok.tipo) << endl;
 
 	} while (tok.tipo != TK_EOF);
-
-	cout << endl;
 
 	fclose(f);
 
@@ -754,4 +588,178 @@ proximo_token()
 	registro_lexico->push_back(tok);
 
 	return tok;
+}
+
+string
+nome_tipo_token(token_type_t tipo)
+{
+	string tkname = "ERRO";
+	switch (tipo)
+	{
+		case TK_ID:
+			tkname = "ID";
+			break;
+
+		case TK_CONST:
+			tkname = "CONST";
+			break;
+
+		case TK_OP_ATTRIB:
+			tkname = "OP_ATTRIB";
+			break;
+
+		case TK_OP_LT:
+			tkname = "OP_LT";
+			break;
+
+		case TK_OP_GT:
+			tkname = "OP_GT";
+			break;
+
+		case TK_OP_LE:
+			tkname = "OP_LE";
+			break;
+
+		case TK_OP_GE:
+			tkname = "OP_GE";
+			break;
+
+		case TK_OP_EQ:
+			tkname = "OP_EQ";
+			break;
+
+		case TK_OP_NE:
+			tkname = "OP_NE";
+			break;
+
+		case TK_OP_PLUS:
+			tkname = "OP_PLUS";
+			break;
+
+		case TK_OP_MINUS:
+			tkname = "OP_MINUS";
+			break;
+
+		case TK_OP_MUL:
+			tkname = "OP_MUL";
+			break;
+
+		case TK_OP_SLASH:
+			tkname = "OP_SLASH";
+			break;
+
+		case TK_OP_PERCENT:
+			tkname = "OP_PERCENT";
+			break;
+
+		case TK_BRA_O_PAR:
+			tkname = "BRA_O_PAR";
+			break;
+
+		case TK_BRA_C_PAR:
+			tkname = "BRA_C_PAR";
+			break;
+
+		case TK_BRA_O_SQR:
+			tkname = "BRA_O_SQR";
+			break;
+
+		case TK_BRA_C_SQR:
+			tkname = "BRA_C_SQR";
+			break;
+
+		case TK_BRA_O_CUR:
+			tkname = "BRA_O_CUR";
+			break;
+
+		case TK_BRA_C_CUR:
+			tkname = "BRA_C_CUR";
+			break;
+
+		case TK_OP_COMMA:
+			tkname = "OP_COMMA";
+			break;
+
+		case TK_RES_FINAL:
+			tkname = "RES_FINAL";
+			break;
+
+		case TK_RES_INT:
+			tkname = "RES_INT";
+			break;
+
+		case TK_RES_CHAR:
+			tkname = "RES_CHAR";
+			break;
+
+		case TK_RES_BOOLEAN:
+			tkname = "RES_BOOLEAN";
+			break;
+
+		case TK_RES_IF:
+			tkname = "RES_IF";
+			break;
+
+		case TK_RES_ELSE:
+			tkname = "RES_ELSE";
+			break;
+
+		case TK_RES_THEN:
+			tkname = "RES_THEN";
+			break;
+
+		case TK_RES_WHILE:
+			tkname = "RES_WHILE";
+			break;
+
+		case TK_RES_FOR:
+			tkname = "RES_FOR";
+			break;
+
+		case TK_RES_AND:
+			tkname = "RES_AND";
+			break;
+
+		case TK_RES_OR:
+			tkname = "RES_OR";
+			break;
+
+		case TK_RES_NOT:
+			tkname = "RES_NOT";
+			break;
+
+		case TK_RES_FALSE:
+			tkname = "RES_FALSE";
+			break;
+
+		case TK_RES_TRUE:
+			tkname = "RES_TRUE";
+			break;
+
+		case TK_RES_WRITE:
+			tkname = "RES_WRITE";
+			break;
+
+		case TK_RES_WRITELN:
+			tkname = "RES_WRITELN";
+			break;
+
+		case TK_RES_READLN:
+			tkname = "RES_READLN";
+			break;
+
+		case TK_RES_MAIN:
+			tkname = "RES_MAIN";
+			break;
+
+		case TK_END_STATEMENT:
+			tkname = "END_STATEMENT";
+			break;
+
+		case TK_EOF:
+			tkname = "EOF";
+			break;
+	}
+
+	return tkname;
 }
