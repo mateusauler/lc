d07ef27be5f1b638b06396f5d365acc4ce33dea7
Vinícius Viterbo <viniciusvviterbo@protonmail.com>
2021-03-06 17:11:49 -0300

    main+tipos: Criação de arquivo tipos.h
    Declaração de enums separadas para arquivo header

---
+++ src/tipos.h
@@ -0,0 +1,88 @@
+#ifndef TIPOS_H
+#define TIPOS_H
+
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+typedef enum {
+	ST_START, // Inicio
+	ST_END,   // Fim, deve retornar o token
+
+	ST_ID_UNDERSCORE,       // Somente leu _
+	ST_ID_NAME,             // Esta lendo o nome do identificador (max 32 char)
+
+	ST_CONST_HEX_START,     // Leu 0
+	ST_CONST_HEX_ALPHA1,    // Leu um [A-F]
+	ST_CONST_HEX_ALPHA2,    // Leu dois [A-F]
+	ST_CONST_HEX_NUM1,      // Leu 0 seguido de um digito
+	ST_CONST_HEX_NUM2,      // Leu 0 seguido de dois digitos
+	ST_CONST_NUM,           // Esta lendo constante numerica (nao hexa)
+
+	ST_CONST_CHAR_START,    // Leu '
+	ST_CONST_CHAR_INTERNAL, // Leu ' e um caractere imprimivel
+
+	ST_CONST_STR_INTERNAL,  // Leu " e esta lendo caracteres de string
+
+	ST_COMMENT,             // Leu /* e esta lendo comentario
+	ST_COMMENT_END,         // Leu * e pode ler /, terminando o comentario
+	ST_COMMENT_NEWLINE,     // Leu \r e pode ler um \n (dentro do comentario, deve contabilizar nova linha)
+
+	ST_NEWLINE,             // Leu \r e pode ler um \n (deve incrementar o contador de linha)
+
+	ST_OP_SLASH,            // Leu /
+	ST_OP_ATTRIB_START,     // Leu :
+	ST_OP_LT,               // Leu <
+	ST_OP_GT,               // Leu >
+} state_t;
+
+typedef enum {
+	TK_ID,           // Identificador
+	TK_CONST,        // Constante
+
+	TK_OP_ATTRIB,     // :=
+
+	TK_OP_LT,         // <
+	TK_OP_GT,         // >
+	TK_OP_LE,         // <=
+	TK_OP_GE,         // >=
+	TK_OP_EQ,         // =
+	TK_OP_NE,         // <>
+
+	TK_OP_PLUS,       // +
+	TK_OP_MINUS,      // -
+	TK_OP_MUL,        // *
+	TK_OP_SLASH,      // /
+	TK_OP_PERCENT,    // %
+	TK_OP_COMMA,      // ,
+
+	TK_BRA_O_PAR,     // (
+	TK_BRA_C_PAR,     // )
+	TK_BRA_O_SQR,     // [
+	TK_BRA_C_SQR,     // ]
+	TK_BRA_O_CUR,     // {
+	TK_BRA_C_CUR,     // }
+
+	TK_END_STATEMENT, // ;
+	TK_EOF            // EOF
+} token_type_t;
+
+typedef enum {
+	CONST_NULL,
+	CONST_INT,
+	CONST_CHAR,
+	CONST_HEX,
+	CONST_STR,
+	CONST_BOOL
+} const_type_t;
+
+typedef struct {
+	token_type_t tipo;
+	string lex;
+	void* simbolo;
+	const_type_t tipo_constante;
+	int tam_constante;
+} token_t;
+
+#endif
