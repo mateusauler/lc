4776bfae2d1ab9a2227940dd12d723e02b800008
Mateus Auler <mateusauler@protonmail.com>
2021-03-29 15:47:02 -0300

    combineFiles.sh: Combinacao de arquivos e feita utilizando o preprocessador

--- combineFiles.sh
+++ combineFiles.sh
@@ -11,11 +11,18 @@ maincpp=$srcdir/main.cpp
 headers=$(ls $srcdir/*.h | grep -v $mainh)
 code=$(ls $srcdir/*.cpp | grep -v $maincpp)
 
-includes=$(grep -h -E "#include <.+>" $mainh $maincpp $includes $code | sort -u)
+includes=$(grep -h -E "#include <.+>" $mainh $maincpp $headers $code | sort -u)
 
-cat $mainh >> $tmpfile
+cat $mainh > $tmpfile
 
 cat $headers >> $tmpfile
+sed -E -i "/#include \".+\"/d" $tmpfile
+sed -E -i "/#include <.+>/d" $tmpfile
+
+cpp -C $tmpfile > $filename
+
+tail -n +57 $filename > $tmpfile
+
 cat $code >> $tmpfile
 
 cat $maincpp >> $tmpfile
