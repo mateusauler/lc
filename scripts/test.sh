4f2cc9889113b131f5acbbd32529e637058565cf
Mateus Auler <mateusauler@protonmail.com>
2021-04-15 11:24:01 -0300

    testes: Modificada rotina de testes
    - Renomeado diretorio "exemplos" para "testes".
    - Adicionados resultados esperados (extensao .se => saida esperada).
    - Script de testes verifica se a saida obtida bate com a esperada.
    
    TODO: Criar rotina de testes e resultados esperados para o programa gerado.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
 testes="ok lexico sintatico"
+dirfonte=testes/fonte
+diresperado=testes/esperado/compilador
+
+erros=""
+
+function cat_erro()
+{
+    erros="${erros}$1"
+}
 
 for d in $testes; do
-    printf "$d:\n\n"
-    for f in $(ls exemplos/$d); do
-        filename="exemplos/$d/$f"
+    for f in $(ls $dirfonte/$d); do
+        filename="$dirfonte/$d/$f"
         s1=$(./lc $filename)
         s2=$(./lc < $filename)
 
-        printf "$f:\n"
-
         if [ "$s1" = "$s2" ] ; then
-            if [ $(command -v valgrind) ] ; then
-                valgrind --leak-check=full -q ./lc $filename
-            else
-                printf "%b\n" "$s1"
+            fileresultado=$(echo "$diresperado/$d/$f" | sed "s/\.l/.se/")
+            esperado=$(cat $fileresultado)
+            if [ "$s1" != "$esperado" ] ; then
+                cat_erro "\n$filename:\n"
+                cat_erro "Saida inesperada.\n"
+                cat_erro "Esperado:\n"
+                cat_erro "$esperado\n"
+                cat_erro "Gerado:\n"
+                cat_erro "$s1\n\n"
             fi
         else
-            printf "Saidas incompativeis.\n"
-            printf "%b" "$s1\n"
-            printf "%b" "$s2\n"
+            cat_erro "$filename:\n"
+            cat_erro "Saidas incompativeis.\n"
+            cat_erro "$s1\n"
+            cat_erro "$s2\n"
         fi
-        printf "\n"
     done
-    printf "\n"
 done
+
+if [ -z "$erros" ] ; then
+    exit 0
+fi
+
+printf "%b" "$erros"
+exit 1
