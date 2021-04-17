17d941f3313f128d7c7067840857d6457d36d109
Vinícius Viterbo <viniciusvviterbo@protonmail.com>
2021-04-16 21:51:57 -0300

    excessoes: Adicionadas excessões do analisador semântico

--- src/excessoes.h
+++ src/excessoes.h
@@ -36,3 +36,33 @@ class eof_inesperado : public erro_fonte
 public:
     eof_inesperado() : erro_fonte("fim de arquivo nao esperado.") {}
 };
+
+class id_nao_declarado : public erro_fonte
+{
+public:
+    id_nao_declarado(std::string l) : erro_fonte("identificador nao declarado [" + l + "].") {}
+};
+
+class id_ja_declarado : public erro_fonte
+{
+public:
+    id_ja_declarado(std::string l) : erro_fonte("identificador ja declarado [" + l + "].") {}
+};
+
+class classe_id_incompativel : public erro_fonte
+{
+public:
+    classe_id_incompativel(std::string l) : erro_fonte("classe de identificador incompativel [" + l + "].") {}
+};
+
+class tipo_incompativel : public erro_fonte
+{
+public:
+    tipo_incompativel() : erro_fonte("tipos incompativeis.") {}
+};
+
+class tam_vet_excede_max : public erro_fonte
+{
+public:
+    tam_vet_excede_max() : erro_fonte("tamanho do vetor excede o maximo permitido.") {}
+};
