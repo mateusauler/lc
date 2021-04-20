2fcc4e3c1305cdb1e75aeda1c0b8efac74c5d65a
Mateus Auler <mateusauler@protonmail.com>
2021-04-20 17:44:24 -0300

    tabela_hash: Declaracoes separadas das implementacoes

--- src/tabela_hash.h
+++ src/tabela_hash.h
@@ -25,102 +25,117 @@ protected:
     int tamanho_tbl; // Numero de buckets da tabela
 
 public:
-    tabela_hash(int tamanho = 128) : tamanho_tbl(tamanho)
-    {
-        if (tamanho_tbl <= 0)
-            tamanho_tbl = 128;
+    tabela_hash(int tamanho = 128);
 
-        tabela = new hash_bkt<T> *[tamanho];
-
-        // Todos os itens da tabela sao inicialmente um ponteiro nulo
-        memset(tabela, 0, tamanho * sizeof(void*));
-    }
-
-    ~tabela_hash()
-    {
-        // Limpa todos os membros de cada entrada da tabela
-        for (int i = 0; i < tamanho_tbl; ++i)
-        {
-            if (tabela[i])
-            {
-
-                hash_bkt<T> *tmp = tabela[i];
-                hash_bkt<T> *prox;
-
-                while (tmp)
-                {
-                    prox = tmp->prox;
-                    delete tmp->elemento;
-                    delete tmp;
-                    tmp = prox;
-                }
-            }
-        }
-
-        delete[] tabela;
-    }
+    ~tabela_hash();
 
     // Calcula a hash de uma string
-    int calcula_hash(std::string chave)
-    {
-        return std::hash<std::string>()(chave) % tamanho_tbl;
-    }
+    int calcula_hash(std::string chave);
 
     /*
      * Insere um item com conteudo "elemento" e chave "chave".
      * Retorna um ponteiro para o objeto encapsulado.
      */
-    hash_bkt<T>* inserir(std::string chave, T* elemento)
-    {
-        // Calcula a hash da chave
-        int hash_chave = calcula_hash(chave);
-
-        // Encapsula o elemento em um "bucket"
-        hash_bkt<T> *obj = new hash_bkt<T>(elemento, chave);
-
-        if (tabela[hash_chave] == nullptr) // Caso nao exista, sera a nova cabeca
-            tabela[hash_chave] = obj;
-        else
-        {
-            // Navega ate o final da lista para inserir a entrada
-
-            hash_bkt<T>* tmp = tabela[hash_chave];
-
-            while(tmp->prox != nullptr)
-                tmp = tmp->prox;
-
-            tmp->prox = obj;
-        }
-
-        return obj;
-    }
+    hash_bkt<T>* inserir(std::string chave, T* elemento);
 
     /*
      * Busca um elemento com a chave "chave" na tabela.
      * Retorna um ponteiro para o registro na tabela.
      */
-    hash_bkt<T>* buscar(std::string chave)
+    hash_bkt<T>* buscar(std::string chave);
+
+};
+
+template<typename T>
+tabela_hash<T>::tabela_hash(int tamanho) : tamanho_tbl(tamanho)
+{
+    if (tamanho_tbl <= 0)
+        tamanho_tbl = 128;
+
+    tabela = new hash_bkt<T> *[tamanho];
+
+    // Todos os itens da tabela sao inicialmente um ponteiro nulo
+    memset(tabela, 0, tamanho * sizeof(void*));
+}
+
+template<typename T>
+tabela_hash<T>::~tabela_hash()
+{
+    // Limpa todos os membros de cada entrada da tabela
+    for (int i = 0; i < tamanho_tbl; ++i)
     {
-        // Calcula a hash desta chave
-        int hash_chave = calcula_hash(chave);
-
-        // Caso a entrada na posicao esprada esteja vazia, o elemento buscado nao existe
-        if (tabela[hash_chave] == nullptr)
-            return nullptr;
-
-        hash_bkt<T>* tmp = tabela[hash_chave];
-
-        // Busca ate a chave da entrada igualar a buscada, ou chegar ao final da lista
-        while (true)
+        if (tabela[i])
         {
-            if (chave == tmp->chave)
-                return tmp;
 
-            if (tmp->prox == nullptr)
-                return nullptr;
+            hash_bkt<T> *tmp = tabela[i];
+            hash_bkt<T> *prox;
 
-            tmp = tmp->prox;
+            while (tmp)
+            {
+                prox = tmp->prox;
+                delete tmp->elemento;
+                delete tmp;
+                tmp = prox;
+            }
         }
     }
 
-};
+    delete[] tabela;
+}
+
+template<typename T>
+int tabela_hash<T>::calcula_hash(std::string chave)
+{
+    return std::hash<std::string>()(chave) % tamanho_tbl;
+}
+
+template<typename T>
+hash_bkt<T>* tabela_hash<T>::inserir(std::string chave, T *elemento)
+{
+    // Calcula a hash da chave
+    int hash_chave = calcula_hash(chave);
+
+    // Encapsula o elemento em um "bucket"
+    hash_bkt<T> *obj = new hash_bkt<T>(elemento, chave);
+
+    if (tabela[hash_chave] == nullptr) // Caso nao exista, sera a nova cabeca
+        tabela[hash_chave] = obj;
+    else
+    {
+        // Navega ate o final da lista para inserir a entrada
+
+        hash_bkt<T>* tmp = tabela[hash_chave];
+
+        while(tmp->prox != nullptr)
+            tmp = tmp->prox;
+
+        tmp->prox = obj;
+    }
+
+    return obj;
+}
+
+template<typename T>
+hash_bkt<T>* tabela_hash<T>::buscar(std::string chave)
+{
+    // Calcula a hash desta chave
+    int hash_chave = calcula_hash(chave);
+
+    // Caso a entrada na posicao esprada esteja vazia, o elemento buscado nao existe
+    if (tabela[hash_chave] == nullptr)
+        return nullptr;
+
+    hash_bkt<T>* tmp = tabela[hash_chave];
+
+    // Busca ate a chave da entrada igualar a buscada, ou chegar ao final da lista
+    while (true)
+    {
+        if (chave == tmp->chave)
+            return tmp;
+
+        if (tmp->prox == nullptr)
+            return nullptr;
+
+        tmp = tmp->prox;
+    }
+}
