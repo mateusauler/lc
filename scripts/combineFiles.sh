851901b8eeb503955534f888d7b5d4d81689f1ba
Mateus Auler <mateusauler@protonmail.com>
2021-04-21 01:42:47 -0300

    combineFiles: Adiciona cabecalho ao concatenar arquivo
    Indica de qual arquivo veio aquele codigo.

--- scripts/combineFiles.sh
+++ scripts/combineFiles.sh
@@ -11,12 +11,28 @@ code=$(ls $srcdir/*.cpp | grep -v $maincpp)
 
 includes=$(grep -h -E "#include <.+>" $maincpp $headers $code | sort -u)
 
+function manda_arquivo()
+{
+    nome=$(echo $1 | sed "s/$srcdir\///")
+    printf "%b" "\n/*\n * ----------------------------------------\n" >> $2
+    printf "%b" " * $nome\n" >> $2
+    printf "%b" " * ----------------------------------------\n */\n" >> $2
+    cat $1 >> $2
+}
+
 printf "%b" "$includes\n" > $filename
 
-cat $headers > $tmpfile
+[ -f $tmpfile ] && rm $tmpfile
+for f in $headers ; do
+    manda_arquivo $f $tmpfile
+done
+
 sed -i "/#pragma once/d" $tmpfile
 
-cat $code $maincpp >> $tmpfile
+for f in $code ; do
+    manda_arquivo $f $tmpfile
+done
+manda_arquivo $maincpp $tmpfile
 
 sed -E -i "/#include (\".+\"|<.+>)/d" $tmpfile
 
